cf2a164b13a1142447acfb1d5425fa932d912142
Danil Rodionow <danil.rodionow13@gmail.com>
2021-01-20 12:03:05 +0300

    feat: add functions: avg,min,max

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+exports.min = function min(array) {
+    if (array && array.length) return Math.min.apply(null, array);
+    else return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array && array.length) return Math.max.apply(null, array);
+    else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && array.length) return array.reduce((val, item) => val + item) / array.length;
+    else return 0;
+};
